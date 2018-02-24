c07c135bf3c53a168e643015a37079fbd70f29dc
Daniel Evans <migetman9@gmail.com>
2018-02-23 17:15:00 -0800

    Re-add SRC
    just in case

---
+++ src/gatsby-node.js
@@ -0,0 +1,46 @@
+const Airtable = require('airtable')
+const crypto = require(`crypto`)
+
+exports.sourceNodes = async ({ boundActionCreators },
+                             {apiKey, baseId, tableName, tableView, queryName}) => {
+  const { createNode, setPluginStatus } = boundActionCreators
+  const base = new Airtable({
+    apiKey,
+  }).base(baseId);
+  const table = base(tableName);
+
+  const query = await table.select({
+    view: tableView,
+  })
+
+  console.time(`fetch all Airtable rows`)
+  const all = await query.all();
+
+  console.timeEnd(`fetch all Airtable rows`)
+
+  setPluginStatus({
+    status: {
+      lastFetched: new Date().toJSON(),
+    },
+  })
+
+  all.forEach(row => {
+    const gatsbyNode = Object.assign({
+      // Required Gatsby fields
+      id: `${row.id}`,
+      parent: "__SOURCE__",
+      children: [],
+      internal: {
+        type: `Airtable${queryName}`,
+        contentDigest: crypto
+          .createHash("md5")
+          .update(JSON.stringify(row))
+          .digest("hex")
+      }
+    }, row.fields);
+
+    createNode(gatsbyNode);
+  });
+
+  return
+}
